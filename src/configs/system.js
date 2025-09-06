438c3b6898a2d443dfb101b5f7a35c4d1c489bb9
ttvKieran <tatruongvuptit@gmail.com>
2025-09-06 21:57:09 +0700

    change api_base_url

--- src/configs/system.js
+++ src/configs/system.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://edushare-api.vercel.app";
 export default API_BASE_URL;
