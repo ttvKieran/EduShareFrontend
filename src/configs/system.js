652c612d82e3f7657cd1ba7768deb6e72268d4ea
ttvKieran <tatruongvuptit@gmail.com>
2025-09-07 00:31:31 +0700

    update api_url

--- src/configs/system.js
+++ src/configs/system.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:8080";
-// const API_BASE_URL = "https://edushare-d4y7.onrender.com";
+// const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://edushare-d4y7.onrender.com";
 export default API_BASE_URL;
